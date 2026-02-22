80b5ea438f7cb80a5a9a3bcc86474e31adf300a9
harsh-rajput-9104 <harshrajput9104@gmail.com>
2026-02-22 16:41:28 +0530

    Final Changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import { Routes, Route } from 'react-router-dom';
 import IntroPage from './pages/IntroPage';
 import GeneratePage from './pages/GeneratePage';
 import HistoryPage from './pages/HistoryPage';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
     return (
-        <Routes>
-            <Route path="/" element={<IntroPage />} />
-            <Route path="/generate" element={<GeneratePage />} />
-            <Route path="/history" element={<HistoryPage />} />
-        </Routes>
+        <>
+            <ScrollToTop />
+            <Routes>
+                <Route path="/" element={<IntroPage />} />
+                <Route path="/generate" element={<GeneratePage />} />
+                <Route path="/history" element={<HistoryPage />} />
+            </Routes>
+        </>
     );
 }
 
